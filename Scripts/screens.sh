02b36b6a2229d0fa84842783857bd8027c0e4017
Joseph D. Marhee <jmarhee@interiorae.com>
2021-03-08 03:09:27 -0600

    Updates timestamp cutting to be random within the limit of the actual file, not hardcoded into the script so it can be reused outside of my use case that I wrote it for. Still needs support for Hours, but should be trivial, and will be next

--- Scripts/screens.sh
+++ Scripts/screens.sh
@@ -8,11 +8,15 @@ if [ -z "${LNVC_PREV_PATH}" ]; then
     echo "No target path found." ; exit 1
 fi
 
-MINUTE=$(echo $((10 + RANDOM % 40)))
-SECOND=$(echo $((10 + RANDOM % 59)))
-
 if [ -n "${REFRESH_SCREENS}" ]; then
     rm -rf LNVC_PREV_PATH/*.jpg
 fi
 
-for v in `ls $LNVC_DATA_PATH`; do ffmpeg -i $LNVC_DATA_PATH/$v -ss 00:$MINUTE:$SECOND.000 -vframes 1 $LNVC_PREV_PATH/`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`.jpg ; done
+for v in `ls $LNVC_DATA_PATH`; do \
+    LENGTH=$(ffmpeg -i $LNVC_DATA_PATH/$v 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//) ; \
+    MINUTE=$(echo $((1 + RANDOM % $(echo $LENGTH | cut -d ":" -f 2)))) ; \
+    if (( $MINUTE < 10 )); then MINUTE=$(printf "%02d\n" $MINUTE); fi ; \
+    SECOND=$(echo $((1 + RANDOM % 59))) ; \
+    if (( $SECOND < 10 )); then SECOND=$(printf "%02d\n" $SECOND); fi ; \
+    ffmpeg -i $LNVC_DATA_PATH/$v -ss :$MINUTE:$SECOND.000 -vframes 1 $LNVC_PREV_PATH/`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`.jpg ; \
+done
