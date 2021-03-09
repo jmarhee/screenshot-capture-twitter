2a43727646120755fb258ab17e6ffacbe519d3bd
Joseph D. Marhee <jmarhee@interiorae.com>
2021-03-09 03:44:41 -0600

    Updates screens.sh to calculate hour value as well as fixing an issue with which directory gets purged

--- Scripts/screens.sh
+++ Scripts/screens.sh
@@ -9,14 +9,16 @@ if [ -z "${LNVC_PREV_PATH}" ]; then
 fi
 
 if [ -n "${REFRESH_SCREENS}" ]; then
-    rm -rf LNVC_PREV_PATH/*.jpg
+    rm -rf $LNVC_PREV_PATH/*.jpg
 fi
 
 for v in `ls $LNVC_DATA_PATH`; do \
     LENGTH=$(ffmpeg -i $LNVC_DATA_PATH/$v 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//) ; \
+    HOUR=$(echo $((0 + RANDOM % $(echo $LENGTH | cut -d ":" -f 2)))) ; \
+    if (( $HOUR < 10 )); then HOUR=$(printf "%02d\n" $HOUR); fi ; \
     MINUTE=$(echo $((1 + RANDOM % $(echo $LENGTH | cut -d ":" -f 2)))) ; \
     if (( $MINUTE < 10 )); then MINUTE=$(printf "%02d\n" $MINUTE); fi ; \
     SECOND=$(echo $((1 + RANDOM % 59))) ; \
     if (( $SECOND < 10 )); then SECOND=$(printf "%02d\n" $SECOND); fi ; \
-    ffmpeg -i $LNVC_DATA_PATH/$v -ss :$MINUTE:$SECOND.000 -vframes 1 $LNVC_PREV_PATH/`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`.jpg ; \
+    ffmpeg -i $LNVC_DATA_PATH/$v -ss $HOUR:$MINUTE:$SECOND.000 -vframes 1 $LNVC_PREV_PATH/`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`.jpg ; \
 done
