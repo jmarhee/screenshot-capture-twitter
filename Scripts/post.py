df43e802746a121a30e53007f7fce4422a8b0d43
Joseph D. Marhee <jmarhee@users.noreply.github.com>
2021-11-25 00:08:54 -0600

    Update post.py
    Updates with tested shuffling behavior for screenshots directory

--- Scripts/post.py
+++ Scripts/post.py
@@ -7,55 +7,39 @@ else:
     exit(1)
 
 def main():
-
     twitter_auth_keys = { 
-
         "consumer_key"        : os.environ['twitter_consumer_key'],
-
         "consumer_secret"     : os.environ['twitter_consumer_secret'],
-
         "access_token"        : os.environ['twitter_access_token'],
-
         "access_token_secret" : os.environ['twitter_access_token_secret'] 
-
     }
 
- 
-
     auth = tweepy.OAuthHandler(
-
             twitter_auth_keys['consumer_key'],
-
             twitter_auth_keys['consumer_secret']
-
             )
 
     auth.set_access_token(
-
             twitter_auth_keys['access_token'],
-
             twitter_auth_keys['access_token_secret']
-
             )
 
     api = tweepy.API(auth)
-
-   
-
+    
     # Upload image
-    path = SCREEN_SOURCE + random.choice(os.listdir(SCREEN_SOURCE))
+    # path = SCREEN_SOURCE + random.choice(os.listdir(SCREEN_SOURCE))
+    # media = api.media_upload(path)
+    files = os.listdir(SCREEN_SOURCE)
+    for i in range(1,10000):
+        random.shuffle(files)
+    path = SCREEN_SOURCE + "/" + random.choice(files)
     media = api.media_upload(path)
-
  
-
     # Post tweet with image
 
     tweet = ""
 
     post_result = api.update_status(status=tweet, media_ids=[media.media_id])
 
- 
-
 if __name__ == "__main__":
-
     main()
